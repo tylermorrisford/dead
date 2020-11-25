a992dca44faa7b4ff693d543328bc4d950d6dd3a
Tyler Morris Ford <tylerford@gmail.com>
2020-11-25 13:03:33 -0700

    add switch, colors, hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import React, {useState} from 'react';
 import './index.css';
+import Switch from "react-switch";
 
-function App() {
+const App = () => {
+
+const [checked, setChecked] = useState(false);
+const [dead, setDead] = useState(false);
+
+const handleChange = () => {
+  setChecked(!checked);
+  setDead(!dead);
+}
+
+const noStyle = {
+  backgroundColor: 'black',
+  color: 'white'
+}
+const yesStyle = {
+  backgroundColor: '#FFF238',
+  color: '#5E1DF0'
+}
+
   return (
-    <div className="content">
-      No
+    <>
+    <div className="content" style={dead ? yesStyle : noStyle}>
+       {dead ? 'Yes!' : 'No'}
+      <div className="slider">
+      <label style={{fontFamily: 'Optima', fontSize: '16px'}}>
+        {dead ? <><span>he's dead.</span><br/></> : <><span>just for giggles, what if he was dead?  try it</span><br /></>}
+        <Switch onColor='#5E1DF0' onChange={handleChange} checked={checked} />
+      </label>
+      </div>
     </div>
+    </>
   );
 }
 
